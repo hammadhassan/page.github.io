49c84efea713ef5baf2c782b0cec509b3bdf3f03
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-10-06 02:04:18 +0500

    border apply

---
+++ app.js
@@ -0,0 +1,4 @@
+var Name = prompt ("Enter your Name");
+document.getElementById("name").innerHTML=(Name);
+var Name1 = prompt ("Enter your Roll Number");
+document.getElementById("name1").innerHTML=(Name1);
